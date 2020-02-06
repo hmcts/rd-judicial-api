711655add1062b2bd1ba8ee070ce269c92092a69
Reem Ibrahim <reem.ibrahim@hmcts.net>
2020-02-06 13:03:31 +0000

    idam integration related code changes to security configuration and application.yaml

--- src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.judicialapi.configuration;
 
-import java.util.List;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,55 +16,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
-import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
+import uk.gov.hmcts.reform.auth.checker.core.user.User;
+import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.AuthCheckerServiceAndUserFilter;
 
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfiguration  {
 
-    //Replace this static class with the one immediately below once IDAM set up
     @ConfigurationProperties(prefix = "security")
-    @Configuration
-    public static class RestAllApiSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-
-        List<String> anonymousPaths;
-
-        private AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
-
-        public List<String> getAnonymousPaths() {
-            return anonymousPaths;
-        }
-
-        public void setAnonymousPaths(List<String> anonymousPaths) {
-            this.anonymousPaths = anonymousPaths;
-        }
-
-        @Override
-        public void configure(WebSecurity web) {
-            web.ignoring()
-                    .antMatchers(anonymousPaths.toArray(new String[0]));
-        }
-
-
-        public RestAllApiSecurityConfigurationAdapter(RequestAuthorizer<Service> serviceRequestAuthorizer,
-
-                                                      AuthenticationManager authenticationManager) {
-
-            authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
-
-            authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
-        }
-
-        protected void configure(HttpSecurity http) throws Exception {
-
-            http.addFilter(authCheckerServiceOnlyFilter)
-                    .csrf().disable()
-                    .authorizeRequests()
-                    .anyRequest().authenticated();
-        }
-    }
-
-    /*@ConfigurationProperties(prefix = "security")
     @Configuration
     public static class RestAllApiSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 
@@ -120,8 +81,7 @@ public class SecurityConfiguration  {
                     .addFilter(authCheckerServiceAndUserFilter);
         }
 
-
-    }*/
+    }
 
 }
 
