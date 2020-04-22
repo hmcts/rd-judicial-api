087938581c4def93ab6081ac1ffcf0ae9415f53b
abhijit <Abhijit.diwan@hmcts.net>
2020-04-22 15:25:42 +0100

    fix: datasource connection settings updated

--- src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/DataSourceConfig.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/DataSourceConfig.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Value("${spring.datasource.url}")
