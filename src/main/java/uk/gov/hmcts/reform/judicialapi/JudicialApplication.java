ec105f102e5d2fe237e00e31a4c9a4d29a62c031
shreedhar-hmcts <49564702+shreedhar-hmcts@users.noreply.github.com>
2020-08-11 13:07:47 +0100

    RDCC-1695 adding authorisation table in db (#56)
    * refactor: Suppression xml cleanup (RDCC-1336) (#38)
    
    refactor: Suppression xml cleanup (RDCC-1336)
    
    * RDCC-1440: removing installCharts() from Jenkinsfile_CNP & parameterized (#39)
    
    * fix: Addressing vulnerabilities & nightly(RDCC-1399) (#42)
    
    * RDCC-1399
    
    * RDCC-1399: suppressing ant jar
    
    * refactor: commented functional tests in nightly
    
    Co-authored-by: abhijit <Abhijit.diwan@hmcts.net>
    
    * refactor: Removed uniqueness on personal_code user profile (RDCC-1550) (#44)
    
    * refactor: Removed uniqueness on personal_code user profile
    
    * refactor: Removed uniqueness on personal_code user profile
    
    * refactor:Integrate HMCTS Java plugin (RDCC-1568) (#47)
    
    * feat: Java 11 migration
    
    * feat: Java 11 migration
    
    * refactor:Integrate HMCTS Java plugin (RDCC-1568)
    
    * refactor:Integrate HMCTS Java plugin (RDCC-1568)
    
    * refactor:Integrate HMCTS Java plugin (RDCC-1568)
    
    * fix: flyway fix for java 11 (#48)
    
    * Add renovate.json (#1)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
    
    * fix:clean up suppressions (RDCC-1740) (#50)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:smoke test fix (RDCC-1740) (#51)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix: Spring liveness probe update (#52)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:Spring liveness fix (#53)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:clean up suppressions (RDCC-1740)
    
    * fix:PI Mutation plugin update (#54)
    
    * RDCC-1695: Updating auth table
    
    Co-authored-by: choudsc <61413530+choudsc@users.noreply.github.com>
    Co-authored-by: Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
    Co-authored-by: abhijit <Abhijit.diwan@hmcts.net>
    Co-authored-by: abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>

--- src/main/java/uk/gov/hmcts/reform/judicialapi/JudicialApplication.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/JudicialApplication.java
@@ -2,20 +2,9 @@ package uk.gov.hmcts.reform.judicialapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.retry.annotation.EnableRetry;
+import uk.gov.hmcts.reform.health.HealthAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.judicialapi")
-@EnableJpaAuditing
-@EnableJpaRepositories
-@EnableRetry
-@EnableCircuitBreaker
-@EnableFeignClients(basePackages = {
-        "uk.gov.hmcts.reform.judicialapi",
-})
+@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.judicialapi", exclude = HealthAutoConfiguration.class)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class JudicialApplication {
 
