1e614c3cd55eb886ee030696962b633144179801
abhijit <Abhijit.diwan@hmcts.net>
2020-09-29 10:42:24 +0100

    fix:Terraform version fix

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = ">= 0.12"  # Terraform client version
   backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.25"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
 
 data "terraform_remote_state" "core_apps_infrastructure" {
