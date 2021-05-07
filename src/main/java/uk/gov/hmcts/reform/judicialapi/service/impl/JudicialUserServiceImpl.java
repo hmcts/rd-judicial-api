0c3ee5753b1a2bc2acb70922dc73e8d789a87165
Nitish Singla <nitish.singla@hmcts.net>
2021-05-07 17:12:13 +0100

    RDCC-2760 : Logging the query and DTO conversion time.

--- src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/JudicialUserServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/JudicialUserServiceImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.judicialapi.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.reform.judicialapi.util.RefDataUtil.createPageableObject;
 
+@Slf4j
 @Service
 public class JudicialUserServiceImpl implements JudicialUserService {
 
@@ -25,18 +27,22 @@ public class JudicialUserServiceImpl implements JudicialUserService {
     @Value("${defaultPageSize}")
     Integer defaultPageSize;
 
-
     @Override
     public ResponseEntity<Object> fetchJudicialUsers(Integer size, Integer page) {
-
+        long startTimeForQuery = System.currentTimeMillis();
         Pageable pageable = createPageableObject(page, size, defaultPageSize);
         Page<UserProfile> pagedUserProfiles = userProfileRepository.findAll(pageable);
 
-        List<UserProfile> userProfiles = pagedUserProfiles.getContent();
+        log.info("The query took {} milliseconds for {} records: ",
+                System.currentTimeMillis() - startTimeForQuery, size);
 
+        long startTimeForObjectConversion = System.currentTimeMillis();
+        List<UserProfile> userProfiles = pagedUserProfiles.getContent();
         List<OrmResponse> ormResponses = userProfiles.stream()
                 .map(OrmResponse::new)
                 .collect(Collectors.toList());
+        log.info("The object conversion took {} milliseconds: ",
+                System.currentTimeMillis() - startTimeForObjectConversion);
 
         return ResponseEntity
                 .status(200)
