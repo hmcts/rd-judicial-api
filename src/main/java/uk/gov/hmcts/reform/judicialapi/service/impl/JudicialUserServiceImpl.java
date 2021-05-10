2c5e570e72461688c6b2caea1611b1eafa55bf58
ishratt <tunazzina.ishrat@hmcts.net>
2021-05-10 17:42:20 +0100

    RDCC-2760: adding default sorting for pagination

--- src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/JudicialUserServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/JudicialUserServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.judicialapi.controller.response.OrmResponse;
@@ -30,7 +31,8 @@ public class JudicialUserServiceImpl implements JudicialUserService {
     @Override
     public ResponseEntity<Object> fetchJudicialUsers(Integer size, Integer page) {
         long startTimeForQuery = System.currentTimeMillis();
-        Pageable pageable = createPageableObject(page, size, defaultPageSize);
+        Pageable pageable = createPageableObject(page, size, defaultPageSize,
+                Sort.by(Sort.DEFAULT_DIRECTION, "elinksId"));
         Page<UserProfile> pagedUserProfiles = userProfileRepository.findAll(pageable);
 
         log.info("The query took {} milliseconds for {} records: ",
--- src/main/java/uk/gov/hmcts/reform/judicialapi/util/RefDataUtil.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/util/RefDataUtil.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import static java.util.Objects.isNull;
 
@@ -14,11 +15,11 @@ public class RefDataUtil {
     private RefDataUtil() {
     }
 
-    public static Pageable createPageableObject(Integer page, Integer size, Integer defaultPageSize) {
+    public static Pageable createPageableObject(Integer page, Integer size, Integer defaultPageSize, Sort sort) {
         if (isNull(size)) {
             size = defaultPageSize;
         }
-        return PageRequest.of(page, size);
+        return PageRequest.of(page, size, sort);
     }
 
 }
