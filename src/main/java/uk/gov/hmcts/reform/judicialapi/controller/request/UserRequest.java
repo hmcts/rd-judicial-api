9b6d0ab015db59f9e272df91870ff1a927989870
Nitish Singla <nitish.singla@hmcts.net>
2021-05-18 15:00:32 +0100

    RDCC2760 : Removing the JsonProperty.

--- src/main/java/uk/gov/hmcts/reform/judicialapi/controller/request/UserRequest.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/controller/request/UserRequest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.judicialapi.controller.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
