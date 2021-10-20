2c392cc567760638f71c2a0fe727c33ee48d78f1
amitkumarshrivastava <86308925+amitkumarshrivastava@users.noreply.github.com>
2021-10-20 12:13:31 +0100

    RDCC-3448 fixed test cases failure

--- src/main/java/uk/gov/hmcts/reform/judicialapi/repository/UserProfileRepository.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/repository/UserProfileRepository.java
@@ -16,23 +16,23 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, String
     Page<UserProfile> findBySidamIdIn(List<String> sidamIds, Pageable pageable);
 
     @Query(value = "select distinct per "
-            + "from judicial_user_profile per "
-            + "LEFT JOIN FETCH judicial_office_appointment appt "
-            + "on per.perId = appt.perId "
-            + "LEFT JOIN FETCH judicial_office_authorisation auth "
-            + "on per.perId = auth.perId "
-            + "where (per.objectId != '' and per.objectId is not null) "
-            + "and ((appt.endDate >= CURRENT_DATE or appt.endDate is null) "
-            + "or (auth.endDate >= CURRENT_DATE or auth.endDate is null)) "
-            + "and ( (:serviceCode is not null and (lower(appt.serviceCode) = :serviceCode or "
-            + "auth.ticketCode in :ticketCode)) or :serviceCode is null ) "
-            + "and ( :serviceCode = 'bfa1' or ((:locationCode is not null "
-            + "and lower(appt.epimmsId) = :locationCode)"
-            + " or :locationCode is null)) "
-            + "and (lower(per.knownAs) like %:searchString% "
-            + "or lower(per.surname) like %:searchString% "
-            + "or lower(per.fullName)  like %:searchString% "
-            + ")")
+                   + "from judicial_user_profile per "
+                   + "LEFT JOIN FETCH judicial_office_appointment appt "
+                   + "on per.perId = appt.perId "
+                   + "LEFT JOIN FETCH judicial_office_authorisation auth "
+                   + "on per.perId = auth.perId "
+                   + "where (per.objectId != '' and per.objectId is not null) "
+                   + "and ((appt.endDate >= CURRENT_DATE or appt.endDate is null) "
+                   + "or (auth.endDate >= CURRENT_DATE or auth.endDate is null)) "
+                   + "and ( (:serviceCode is not null and (lower(appt.serviceCode) = :serviceCode or "
+                   + "auth.ticketCode in :ticketCode)) or :serviceCode is null ) "
+                   + "and ( :serviceCode = 'bfa1' or ((:locationCode is not null "
+                   + "and lower(appt.epimmsId) = :locationCode)"
+                   + " or :locationCode is null)) "
+                   + "and (lower(per.knownAs) like %:searchString% "
+                   + "or lower(per.surname) like %:searchString% "
+                   + "or lower(per.fullName)  like %:searchString% "
+                   + ")")
     List<UserProfile> findBySearchString(String searchString, String serviceCode, String locationCode,
                                          List<String> ticketCode);
 
