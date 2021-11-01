75ef1b5306251faee52845b2cbe27f9480f20e48
shivajihmcts <shivaji.gaikwad@hmcts.net>
2021-11-01 11:47:09 +0000

    RDCC-3511:  Changes made to code reformating

--- src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/IdamUserProfileServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/IdamUserProfileServiceImpl.java
@@ -6,14 +6,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.judicialapi.controller.feign.IdamUserFeignClient;
+import uk.gov.hmcts.reform.judicialapi.controller.request.RoleDetail;
 import uk.gov.hmcts.reform.judicialapi.controller.request.TestUserRequest;
 import uk.gov.hmcts.reform.judicialapi.controller.response.IdamUserProfileResponse;
+import uk.gov.hmcts.reform.judicialapi.domain.UserProfile;
 import uk.gov.hmcts.reform.judicialapi.repository.IdamUserProfileRepository;
 import uk.gov.hmcts.reform.judicialapi.service.IdamUserProfileService;
-import uk.gov.hmcts.reform.judicialapi.util.RefDataUtil;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.reform.judicialapi.util.RefDataUtil.DEFAULT_USER_PASSWORD;
+import static uk.gov.hmcts.reform.judicialapi.util.RefDataUtil.DEFAULT_USER_ROLE;
 
 
 @Slf4j
@@ -37,7 +42,7 @@ public class IdamUserProfileServiceImpl implements IdamUserProfileService {
 
         var idamTestUsers = userProfiles
                 .stream()
-                .map(RefDataUtil::createTestUser)
+                .map(this::createTestUser)
                 .collect(Collectors.toUnmodifiableList());
 
         var idamUserProfileResponses = new ArrayList<IdamUserProfileResponse>();
@@ -51,12 +56,12 @@ public class IdamUserProfileServiceImpl implements IdamUserProfileService {
                 var idamUserFeignResponse = idamUserFeignClient.createUserProfile(idamUser);
 
                 if(HttpStatus.CREATED.equals(idamUserFeignResponse.status())){
-                    var idamUserProfileSuccessResponse = RefDataUtil.createIdamUserProfileResponse(idamUser);
+                    var idamUserProfileSuccessResponse = createIdamUserProfileResponse(idamUser);
                     idamUserProfileSuccessResponse.setMessage(IDAM_USER_CREATED_SUCCESS);
                     idamUserProfileResponses.add(idamUserProfileSuccessResponse);
                 }
             }catch (Exception exception){
-                var idamUserProfileFailureResponse = RefDataUtil.createIdamUserProfileResponse(idamUser);
+                var idamUserProfileFailureResponse = createIdamUserProfileResponse(idamUser);
                 idamUserProfileFailureResponse.setMessage(IDAM_USER_CREATED_FAIL);
                 idamUserProfileResponses.add(idamUserProfileFailureResponse);
             }
@@ -65,6 +70,39 @@ public class IdamUserProfileServiceImpl implements IdamUserProfileService {
         return ResponseEntity
                 .status(200)
                 .body(idamUserProfileResponses);
+    }
+
+    public  TestUserRequest createTestUser(UserProfile userProfile){
+        TestUserRequest accountDetails = new TestUserRequest();
+
+        accountDetails.setEmail(userProfile.getEjudiciaryEmailId());
+        accountDetails.setForename(userProfile.getFullName());
+        accountDetails.setPassword(DEFAULT_USER_PASSWORD);
+        accountDetails.setId(userProfile.getObjectId());
+
+        accountDetails.setSurname(userProfile.getSurname());
+
+
+        List<RoleDetail> roleCodes = new ArrayList<>();
+        RoleDetail roleCode;
+        roleCode = new RoleDetail();
+        roleCode.setCode(DEFAULT_USER_ROLE);
+        roleCodes.add(roleCode);
+
+        accountDetails.setRoles(roleCodes);
+
+        return accountDetails;
+    }
+
+    public  IdamUserProfileResponse createIdamUserProfileResponse(TestUserRequest userProfile){
+
+        IdamUserProfileResponse idamUserProfileResponse = new IdamUserProfileResponse();
+
+        idamUserProfileResponse.setSurname(userProfile.getSurname());
+        idamUserProfileResponse.setFullName(userProfile.getForename());
+        idamUserProfileResponse.setEmailId(userProfile.getEmail());
+
+        return idamUserProfileResponse;
     }
 
 }
