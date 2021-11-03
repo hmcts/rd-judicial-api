66419cc27367adf24102fcec18dd6985b0e1997e
Sourav Roy <sourav.roy@hmcts.net>
2021-11-03 17:06:45 +0000

    upgrade from Junit4 to Junit5

--- src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/JudicialUsersFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/JudicialUsersFunctionalTest.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.judicialapi;
 import lombok.extern.slf4j.Slf4j;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.judicialapi.controller.advice.ErrorResponse;
 import uk.gov.hmcts.reform.judicialapi.controller.request.UserRequest;
@@ -15,59 +15,61 @@ import uk.gov.hmcts.reform.judicialapi.util.ToggleEnable;
 import java.util.ArrayList;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RunWith(CustomSerenityRunner.class)
 @WithTags({@WithTag("testType:Functional")})
 @Slf4j
-public class JudicialUsersFunctionalTest extends AuthorizationFunctionalTest {
+class JudicialUsersFunctionalTest extends AuthorizationFunctionalTest {
 
     public static final String FETCH_USERS = "JrdUsersController.fetchUsers";
     public static final String USERS_SEARCH = "JrdUsersController.searchUsers";
 
     @Test
     @ToggleEnable(mapKey = FETCH_USERS, withFeature = true)
-    public void shouldReturnDataNotFoundWhenUserProfilesDoNotExistForGivenUserId() {
+    void shouldReturnDataNotFoundWhenUserProfilesDoNotExistForGivenUserId() {
         ErrorResponse errorResponse = (ErrorResponse)
                 judicialApiClient.fetchUserProfiles(getDummyUserRequest(), 10, 0, NOT_FOUND,
                         ROLE_JRD_SYSTEM_USER);
 
-        assertThat(errorResponse).isNotNull();
+        assertNotNull(errorResponse);
     }
 
     @Test
     @ToggleEnable(mapKey = FETCH_USERS, withFeature = true)
-    public void shouldThrowForbiddenExceptionForNonCompliantRole() {
+    void shouldThrowForbiddenExceptionForNonCompliantRole() {
         ErrorResponse errorResponse = (ErrorResponse)
                 judicialApiClient.fetchUserProfiles(getDummyUserRequest(), 10, 0, FORBIDDEN,
                         "prd-admin");
 
-        assertThat(errorResponse).isNotNull();
+        assertNotNull(errorResponse);
     }
 
     @Test
     @ToggleEnable(mapKey = FETCH_USERS, withFeature = false)
-    public void shouldGet403WhenApiToggledOff() {
-        String exceptionMessage = CustomSerenityRunner.getFeatureFlagName().concat(" ")
+    void shouldGet403WhenApiToggledOff() {
+        final String exceptionMessage = CustomSerenityRunner.getFeatureFlagName().concat(" ")
                 .concat(FeatureConditionEvaluation.FORBIDDEN_EXCEPTION_LD);
 
         ErrorResponse errorResponse = (ErrorResponse)
                 judicialApiClient.fetchUserProfiles(getDummyUserRequest(), 10, 0, FORBIDDEN,
                         ROLE_JRD_SYSTEM_USER);
 
-        assertThat(errorResponse).isNotNull();
-        assertThat(errorResponse.getErrorMessage()).isEqualTo(exceptionMessage);
+        assertNotNull(errorResponse);
+        assertEquals(exceptionMessage, errorResponse.getErrorMessage());
     }
 
     @Test
     @ToggleEnable(mapKey = USERS_SEARCH, withFeature = false)
-    public void shouldGet403WhenUserSearchApiToggledOff() {
+    void shouldGet403WhenUserSearchApiToggledOff() {
         var errorResponse = (ErrorResponse)
                 judicialApiClient.userSearch(getUserSearchRequest(null, null, "test"),
                         ROLE_JRD_SYSTEM_USER, FORBIDDEN);
-        assertThat(errorResponse).isNotNull();
+        assertNotNull(errorResponse);
     }
 
     private UserRequest getDummyUserRequest() {
