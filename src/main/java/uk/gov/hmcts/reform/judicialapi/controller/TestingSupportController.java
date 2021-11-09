b45a09102de6aa4b6b6a6a114e7687a72b304a6c
shivajihmcts <shivaji.gaikwad@hmcts.net>
2021-11-09 19:32:27 +0000

    RDCC-3511_new: Removed roles from the test controller

--- src/main/java/uk/gov/hmcts/reform/judicialapi/controller/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/controller/TestingSupportController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
