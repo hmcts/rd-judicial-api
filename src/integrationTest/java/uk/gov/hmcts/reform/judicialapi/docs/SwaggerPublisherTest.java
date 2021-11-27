bd7cd4de0d7d87c4b633ee8f3ba8564e9bbce8e1
ManishParyani <manish.paryani@hmcts.net>
2021-11-27 18:30:14 +0000

    junit4 to 5 migration

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/docs/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/docs/SwaggerPublisherTest.java
@@ -1,18 +1,23 @@
 package uk.gov.hmcts.reform.judicialapi.docs;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 import uk.gov.hmcts.reform.judicialapi.JudicialApplication;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
  * Built-in feature which saves service's swagger specs in temporary directory.
@@ -22,28 +27,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class SwaggerPublisherTest {
 
-    private static final String SWAGGER_DOC_JSON_FILE = "/tmp/swagger-specs.json";
+    private MockMvc mvc;
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebApplicationContext webApplicationContext;
 
+    @BeforeEach
+    public void setUp() {
+        this.mvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @DisplayName("Generate swagger documentation")
     @Test
-    void shouldGenerateDocs() throws Exception {
-
-        File linuxTmpDir = new File("/tmp");
-        if (!linuxTmpDir.exists()) {
-            return;
-        }
-
-        byte[] specs = mockMvc.perform(get("/v2/api-docs"))
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void generateDocs() throws Exception {
+        byte[] specs = mvc.perform(get("/v2/api-docs"))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
                 .getContentAsByteArray();
 
-        try (FileOutputStream outputStream = new FileOutputStream(SWAGGER_DOC_JSON_FILE)) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
             outputStream.write(specs);
         }
+
     }
 
 }
