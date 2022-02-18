986c2e0482ca14a0d56ccfb357464c3b8ce7de4c
shivajilokare <shivaji.lokare@HCTS.NET>
2022-02-18 10:35:02 +0000

    added delete query for ticket and service code

---
+++ src/main/resources/db/migration/V1_30_Update_service_ticket_code_for_sscs.sql
@@ -0,0 +1,3 @@
+delete from judicial_service_code_mapping where service_id ='21' or service_id ='55'
+delete from  judicial_ticket_code_mapping where ticket_code ='363' or ticket_code ='376'
+commit;
