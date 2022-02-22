5ba68451d781d3bb439b860eeead0a36d31773aa
shivajilokare <shivaji.lokare@HCTS.NET>
2022-02-22 12:13:51 +0000

    removed spaces

--- src/main/resources/db/migration/V1_29__insert_sscs_record.sql
+++ src/main/resources/db/migration/V1_29__insert_sscs_record.sql
@@ -2,7 +2,3 @@ INSERT INTO judicial_location_mapping (epimms_id, judicial_base_location_id, bui
 DELETE FROM judicial_service_code_mapping WHERE service_id ='21' OR service_id ='55';
 DELETE FROM judicial_ticket_code_mapping WHERE ticket_code ='363' OR ticket_code ='376';
 COMMIT;
-
-
-
-
