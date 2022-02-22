4129df8646f2e5f5120630b10e33be213168f158
shivajilokare <shivaji.lokare@HCTS.NET>
2022-02-22 11:44:58 +0000

    updated corret query format

--- src/main/resources/db/migration/V1_29__insert_sscs_record.sql
+++ src/main/resources/db/migration/V1_29__insert_sscs_record.sql
@@ -1,2 +1,5 @@
-INSERT INTO judicial_location_mapping (judicial_base_location_id,base_location_name,epimms_id,building_location_name, service_code) VALUES ('1032','Social Entitlement','','','BBA3');
+INSERT INTO dbjuddata.judicial_location_mapping(epimms_id, judicial_base_location_id, building_location_name, base_location_name, service_code)VALUES('', '1032', '', 'Social Entitlement', 'BBA3');
 COMMIT;
+
+
+
