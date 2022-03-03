8a4647ddc677f90893b91149c4ab39a992e8dcf4
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-03-03 10:56:41 +0000

    merged the alter tables - RDCC-4150 and RDCC 4148

--- src/main/resources/db/migration/V1_30__alter_judicial_user_profile.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE judicial_user_profile ADD COLUMN is_judge Boolean;
-ALTER TABLE judicial_user_profile ADD COLUMN is_panel_member Boolean;
-ALTER TABLE judicial_user_profile ADD COLUMN is_magistrate Boolean;
-ALTER TABLE judicial_user_profile ADD COLUMN mrd_created_time Timestamp;
-ALTER TABLE judicial_user_profile ADD COLUMN mrd_updated_time Timestamp;
-ALTER TABLE judicial_user_profile ADD COLUMN mrd_deleted_time Timestamp;
-COMMIT;
