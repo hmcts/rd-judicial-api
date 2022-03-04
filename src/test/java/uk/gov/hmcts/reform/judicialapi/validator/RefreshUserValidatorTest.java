32a5d38ab511f72017b14732bdb432db49f944fe
SujeethKumarAri <73644938+SujeethKumarAri@users.noreply.github.com>
2022-03-04 14:57:06 +0000

    fix: Added tests for mutation test coverage

--- src/test/java/uk/gov/hmcts/reform/judicialapi/validator/RefreshUserValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/validator/RefreshUserValidatorTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 class RefreshUserValidatorTest {
 
