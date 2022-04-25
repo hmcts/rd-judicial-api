fe21acb54eecbd8d79dfa485c5c0f24e3265f96b
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2022-04-25 15:51:45 +0100

    java 17 mutation fix

--- src/test/java/uk/gov/hmcts/reform/judicialapi/controller/util/FeatureConditionEvaluationTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/controller/util/FeatureConditionEvaluationTest.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.context.request.RequestContextHolder;
