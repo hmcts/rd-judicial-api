8178bb8962785960de3a7b1e500cb109f5396406
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-05-26 16:57:51 +0100

    RDCC-4680:Changes made to addfilter and exception handling for  500 error.

--- src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,7 +42,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${oidc.issuer}")
     private String issuerOverride;
 
-    private ServiceAuthFilter serviceAuthFilter;
+    @Order(1)
+    private  ServiceAuthFilter serviceAuthFilter;
+    @Order(2)
+    private final SecurityEndpointFilter securityEndpointFilter;
     List<String> anonymousPaths;
 
     private JwtAuthenticationConverter jwtAuthenticationConverter;
@@ -65,12 +69,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Inject
     public SecurityConfiguration(final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter,
                                  final ServiceAuthFilter serviceAuthFilter,
-                                 RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
+                                 RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+                                 SecurityEndpointFilter securityEndpointFilter) {
 
         this.serviceAuthFilter = serviceAuthFilter;
         this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
         jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        this.securityEndpointFilter = securityEndpointFilter;
 
     }
 
