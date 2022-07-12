ad172f06d4bfe3952b427b510e5873cbab4c1b5a
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-07-12 06:52:07 +0100

    RDCC-5070 fixed checkstyle issue

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/SearchUsersIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/SearchUsersIntegrationTest.java
@@ -135,8 +135,8 @@ class SearchUsersIntegrationTest extends AuthorizationEnabledIntegrationTest {
                 userSearchRequest, role, false);
         assertThat(response).containsEntry("http_status", "400");
         var responseBody = (String) response.get("response_body");
-        assertTrue(responseBody.contains("searchString should contains atleast 3 characters with letters, Apostrophe and Hyphen "
-                + "only and white space allowed"));
+        assertTrue(responseBody.contains("searchString should contains atleast 3 characters with letters, Apostrophe" +
+                " and Hyphen only and white space allowed"));
     }
 
     @ParameterizedTest
@@ -151,8 +151,8 @@ class SearchUsersIntegrationTest extends AuthorizationEnabledIntegrationTest {
                 userSearchRequest, role, false);
         assertThat(response).containsEntry("http_status", "400");
         var responseBody = (String) response.get("response_body");
-        assertTrue(responseBody.contains("searchString should contains atleast 3 characters with letters, Apostrophe and Hyphen"
-                + " only and white space allowed"));
+        assertTrue(responseBody.contains("searchString should contains atleast 3 characters with letters, Apostrophe"
+                + " and Hyphen only and white space allowed"));
     }
 
     @ParameterizedTest
