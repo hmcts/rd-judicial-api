2a030fc198c7b3baf037a35496356ebd1d477dc3
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2022-09-27 11:27:59 +0100

    Adding deleted line

--- security.sh
+++ security.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo ${TEST_URL}
 echo "ZAP has successfully started"
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -J report.json -r api-report.html
