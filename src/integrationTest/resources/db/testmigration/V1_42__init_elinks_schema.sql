be5baa4f8ea81544bf676b5799252dfec41fd138
m_659064 <vilas.shelke@HMCTS.NET>
2022-11-21 17:01:28 +0000

    RDCC-5868 eLinks : Create new mapping tables for Judicial Reference Data

--- src/integrationTest/resources/db/testmigration/V1_42__init_elinks_schema.sql
+++ src/integrationTest/resources/db/testmigration/V1_42__init_elinks_schema.sql
@@ -1,3 +1,5 @@
+-- NB Flyway requires lowercase for table names
+
 create schema if not exists dbjudicialdata;
 
 -- dbjudicialdata.judicial_location_mapping definition
@@ -39,7 +41,7 @@ CREATE TABLE dbjudicialdata.judicial_ticket_code_mapping (
 
 -- dbjudicialdata.dataload_schedular_audit definition
 CREATE TABLE dbjudicialdata.dataload_schedular_audit (
-	id serial4 NOT NULL,
+	id serial NOT NULL,
 	scheduler_name varchar(64) NOT NULL,
 	scheduler_start_time timestamp NOT NULL,
 	scheduler_end_time timestamp NULL,
