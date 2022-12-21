9a33c9792e076b22b599d5eee8f2a289d424e0dc
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-21 15:32:58 +0000

    RDCC-6063:Changes made to setup initial eLinks framework

---
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/ElinksController.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.reform.judicialapi.elinks.controller;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.judicialapi.elinks.response.ElinkLocationWrapperResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.BAD_REQUEST;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.FORBIDDEN_ERROR;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.INTERNAL_SERVER_ERROR;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.LOCATION_DATA_LOAD_SUCCESS;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.NO_DATA_FOUND;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.TOO_MANY_REQUESTS;
+import static uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants.UNAUTHORIZED_ERROR;
+
+@RestController
+@RequestMapping(
+        path = "/refdata/jinternal/elink"
+)
+@Slf4j
+@AllArgsConstructor
+@SuppressWarnings("all")
+public class ElinksController {
+
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "Get list of location and populate region type.",
+                    response = ElinkLocationWrapperResponse.class
+            ),
+            @ApiResponse(
+                    code = 400,
+                    message = BAD_REQUEST
+            ),
+            @ApiResponse(
+                    code = 401,
+                    message = UNAUTHORIZED_ERROR
+            ),
+            @ApiResponse(
+                    code = 403,
+                    message = FORBIDDEN_ERROR
+            ),
+            @ApiResponse(
+                    code = 404,
+                    message = NO_DATA_FOUND
+            ),
+            @ApiResponse(
+                    code = 429,
+                    message = TOO_MANY_REQUESTS
+            ),
+            @ApiResponse(
+                    code = 500,
+                    message = INTERNAL_SERVER_ERROR
+            )
+    })
+    @GetMapping(path = "/reference_data/location",
+            produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<ElinkLocationWrapperResponse> loadLocation(){
+
+
+        ElinkLocationWrapperResponse elinkLocationWrapperResponse = new ElinkLocationWrapperResponse();
+        elinkLocationWrapperResponse.setMessage(LOCATION_DATA_LOAD_SUCCESS);
+
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(elinkLocationWrapperResponse);
+    }
+}
