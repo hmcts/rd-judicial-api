e3671e743f0f8f0311bc290c1bfa787974ce5a58
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-01-06 19:01:32 +0000

    dev changes

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ELinksServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ELinksServiceImpl.java
@@ -342,8 +342,8 @@ public class ELinksServiceImpl implements ELinksService {
 
         List<Triple<String, String,String>> leaversId = new ArrayList<>();
 
-        String updateLeaversId = "UPDATE dbjudicialdata.judicial_user_profile SET last-working-date = ? , "
-                + " active-flag = ? WHERE personal_code = ?";
+        String updateLeaversId = "UPDATE dbjudicialdata.judicial_user_profile SET last_working_date = ? , "
+                + "active_flag = ? WHERE personal_code = ?";
 
         resultsRequests.stream().filter(request -> nonNull(request.getPersonalCode())).forEach(s ->
                 leaversId.add(Triple.of(s.getPersonalCode(), s.getLeaver(),s.getLeftOn())));
