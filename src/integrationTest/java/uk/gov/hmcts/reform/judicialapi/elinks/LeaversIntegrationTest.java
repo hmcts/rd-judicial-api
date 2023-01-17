f875f94c4157a0eea51dc08d01e37fc19b4d5e37
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-01-17 17:07:13 +0000

    commit

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/LeaversIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/LeaversIntegrationTest.java
@@ -15,6 +15,9 @@ import uk.gov.hmcts.reform.judicialapi.elinks.util.RefDataElinksConstants;
 import java.util.List;
 import java.util.Map;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -93,4 +96,42 @@ class LeaversIntegrationTest extends ElinksEnabledIntegrationTest {
         assertNotNull(auditEntry.getSchedulerStartTime());
         assertNotNull(auditEntry.getSchedulerEndTime());
     }
+
+    @DisplayName("Elinks Leavers to JRD Audit Functionality verification")
+    @Test
+    void verifyLeaversJrdAuditFunctionalityBadRequestScenario() {
+          elinks.stubFor(get(urlPathMatching("/leavers"))
+                .willReturn(aResponse()
+                        .withStatus(400)
+                        .withHeader("Content-Type", "application/json")
+                        .withHeader("Connection", "close")
+                        .withBody("{"
+
+                                + " }")));
+        Map<String, Object> response = elinksReferenceDataClient.getPeoples();
+        Map<String, Object> leaversResponse = elinksReferenceDataClient.getLeavers();
+        assertThat(leaversResponse).containsEntry("http_status", "400 OK");
+        ElinkLeaversWrapperResponse profiles = (ElinkLeaversWrapperResponse)leaversResponse.get("body");
+        assertEquals("Leavers Data Loaded Successfully", profiles.getMessage());
+
+        List<UserProfile> userprofile = profileRepository.findAll();
+
+        assertEquals(1, userprofile.size());
+        assertEquals("0049931063", userprofile.get(0).getPersonalCode());
+        assertEquals("2021-02-24", userprofile.get(0).getLastWorkingDate().toString());
+        assertEquals(false, userprofile.get(0).getActiveFlag());
+        assertEquals("552da697-4b3d-4aed-9c22-1e903b70aead", userprofile.get(0).getObjectId());
+
+        List<ElinkDataSchedularAudit>  elinksAudit = elinkSchedularAuditRepository.findAll();
+
+        ElinkDataSchedularAudit auditEntry = elinksAudit.get(0);
+
+        assertEquals(1, auditEntry.getId());
+        assertEquals(LEAVERSAPI, auditEntry.getApiName());
+        assertEquals(RefDataElinksConstants.JobStatus.SUCCESS.getStatus(), auditEntry.getStatus());
+        assertEquals(JUDICIAL_REF_DATA_ELINKS, auditEntry.getSchedulerName());
+        assertNotNull(auditEntry.getSchedulerStartTime());
+        assertNotNull(auditEntry.getSchedulerEndTime());
+    }
+
 }
