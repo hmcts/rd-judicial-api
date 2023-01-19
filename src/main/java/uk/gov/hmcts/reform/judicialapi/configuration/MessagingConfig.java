f7f2c46ab7ae14e5707831679501d148f6ddc471
hmcts <prudhvi.maddineni@hmcts.net>
2023-01-19 14:06:26 +0000

    Service Bus enabled

---
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/MessagingConfig.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.judicialapi.configuration;
+
+import com.azure.core.amqp.AmqpRetryOptions;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Configuration
+public class MessagingConfig {
+
+    @Value("${jrd.publisher.azure.service.bus.host}")
+    String host;
+
+    @Value("${jrd.publisher.azure.service.bus.topic}")
+    String topic;
+
+    @Value("${jrd.publisher.azure.service.bus.username}")
+    String sharedAccessKeyName;
+
+    @Value("${jrd.publisher.azure.service.bus.password}")
+    String sharedAccessKeyValue;
+
+    @Bean
+    public ServiceBusSenderClient getServiceBusSenderClient() {
+
+        log.error(topic,sharedAccessKeyName,sharedAccessKeyValue);
+        log.error("Host : ",host);
+        String connectionString = "Endpoint=sb://"
+                + host + ";SharedAccessKeyName=" + sharedAccessKeyName + ";SharedAccessKey=" + sharedAccessKeyValue;
+        log.error(connectionString);
+        return new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .retryOptions(new AmqpRetryOptions())
+                .sender()
+                .topicName(topic)
+                .buildClient();
+    }
+
+
+}
