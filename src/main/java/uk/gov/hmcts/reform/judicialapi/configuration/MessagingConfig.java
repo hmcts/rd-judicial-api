cab6f4819addeb87fa6eea2de7cf0aa0c1733bec
hmcts <prudhvi.maddineni@hmcts.net>
2023-01-20 11:23:37 +0000

    message remove

--- src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/MessagingConfig.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/configuration/MessagingConfig.java
@@ -7,11 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
-@Service
-@Slf4j
 @Configuration
+@Slf4j
 public class MessagingConfig {
 
     @Value("${jrd.publisher.azure.service.bus.host}")
