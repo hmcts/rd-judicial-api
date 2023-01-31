c72f7857a2eba923bed07324a4580167c02d3ddb
shivajihmcts <shivaji.gaikwad@hmcts.net>
2023-01-31 13:33:14 +0000

    RDCC-6222:Changes made to fix failing test cases after merge conflicts

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/NegativeIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/NegativeIntegrationTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.judicialapi.elinks;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
