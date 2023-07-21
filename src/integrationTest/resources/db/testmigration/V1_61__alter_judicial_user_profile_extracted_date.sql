d905e91d5ddb0c0f7ded3a284427c2ca25f68bb8
hmcts <prudhvi.maddineni@hmcts.net>
2023-07-21 13:04:00 +0100

    modifications

--- src/integrationTest/resources/db/testmigration/V1_61__alter_judicial_user_profile_extracted_date.sql
+++ src/integrationTest/resources/db/testmigration/V1_61__alter_judicial_user_profile_extracted_date.sql
@@ -1,9 +1,2 @@
 ALTER TABLE judicial_user_profile ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
 ALTER TABLE judicial_office_appointment ALTER COLUMN extracted_date TYPE date USING extracted_date::date;
-
-
-
---Alter dbjudicialdata.judicial_user_profile
-ALTER TABLE dbjudicialdata.judicial_user_profile ADD COLUMN deleted_flag boolean;
-ALTER TABLE dbjudicialdata.judicial_user_profile ADD COLUMN date_of_deletion TIMESTAMP;
-
