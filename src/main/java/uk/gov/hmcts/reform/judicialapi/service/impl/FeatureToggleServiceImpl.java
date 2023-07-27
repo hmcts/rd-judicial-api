5ad523354f4da89c460b8fc387711c87d355719f
hmcts <prudhvi.maddineni@hmcts.net>
2023-07-27 07:17:36 +0100

    remove base location

--- src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/FeatureToggleServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/service/impl/FeatureToggleServiceImpl.java
@@ -43,12 +43,12 @@ public class FeatureToggleServiceImpl implements FeatureToggleService {
         launchDarklyMap.put("JrdUsersController.searchUsers", judApi);
         launchDarklyMap.put("TestingSupportController.createIdamUserProfiles", "rd-judicial-api-test-idam-users");
         launchDarklyMap.put("JrdUsersController.refreshUserProfile", judApi);
-        launchDarklyMap.put("ElinksController.loadLocation", judApi);
-        launchDarklyMap.put("ElinksController.loadPeople", judApi);
-        launchDarklyMap.put("ElinksController.idamElasticSearch", judApi);
-        launchDarklyMap.put("ElinksController.loadLeavers", judApi);
-        launchDarklyMap.put("ElinksController.loadDeleted", judApi);
-        launchDarklyMap.put("ElinksController.publishSidamIdToAsb", judApi);
+        launchDarklyMap.put("ElinksController.loadLocation", "jrd-elinks-location");
+        launchDarklyMap.put("ElinksController.loadPeople", "jrd-elinks-load-people");
+        launchDarklyMap.put("ElinksController.idamElasticSearch", "jrd-elinks-idam-elastic-search");
+        launchDarklyMap.put("ElinksController.loadLeavers", "jrd-elinks-leavers");
+        launchDarklyMap.put("ElinksController.loadDeleted", "jrd-elinks-load-deleted");
+        launchDarklyMap.put("ElinksController.publishSidamIdToAsb", "jrd-elinks-publish-service-bus");
     }
 
     @Override
