7a63ce560aeb7d12f77dbc55b5cb8d43acf6e478
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-07-30 18:46:09 +0100

    unit test for service implementation

--- src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinkUserServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/judicialapi/elinks/service/impl/ElinkUserServiceImplTest.java
@@ -1,18 +1,24 @@
 package uk.gov.hmcts.reform.judicialapi.elinks.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.judicialapi.elinks.controller.advice.InvalidRequestException;
+import uk.gov.hmcts.reform.judicialapi.elinks.controller.request.RefreshRoleRequest;
 import uk.gov.hmcts.reform.judicialapi.elinks.repository.ProfileRepository;
 import uk.gov.hmcts.reform.judicialapi.elinks.response.UserSearchResponseWrapper;
 import uk.gov.hmcts.reform.judicialapi.repository.ServiceCodeMappingRepository;
-import uk.gov.hmcts.reform.judicialapi.validator.RefreshUserValidator;
+import uk.gov.hmcts.reform.judicialapi.elinks.validator.ElinksRefreshUserValidator;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -31,16 +37,18 @@ class ElinkUserServiceImplTest {
     @Mock
     ServiceCodeMappingRepository serviceCodeMappingRepository;
 
-    private RefreshUserValidator refreshUserValidatorMock;
+    private ElinksRefreshUserValidator refreshUserValidatorMock;
     ObjectMapper mapper = new ObjectMapper();
 
     private List<String> searchServiceCode;
 
     @BeforeEach
     void setUp() {
-        refreshUserValidatorMock = new RefreshUserValidator();
-        searchServiceCode = (List.of("bfa1","bba3"));
-        //elinkUserService.setSearchServiceCode(searchServiceCode);
+         refreshUserValidatorMock = new ElinksRefreshUserValidator();
+         elinkUserService.setElinksRefreshUserValidator(refreshUserValidatorMock);
+
+        //searchServiceCode = (List.of("bfa1","bba3"));
+         //elinkUserService.setSearchServiceCode(searchServiceCode);
     }
 
 
@@ -137,5 +145,77 @@ class ElinkUserServiceImplTest {
 
         return userSearchResponse;
     }
+
+    @Test
+    void test_refreshUserProfile_Two_Input_01() throws JsonProcessingException {
+
+        var refreshRoleRequest = new RefreshRoleRequest("cmc",
+                null, Arrays.asList("test", "test"),null);
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+
+    }
+
+    @Test
+    void test_refreshUserProfile_Two_Input_02() throws JsonProcessingException {
+
+        var refreshRoleRequest = new RefreshRoleRequest("cmc",
+                Arrays.asList("test", "test"), null,null);
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+    }
+
+    @Test
+    void test_refreshUserProfile_Two_Input_03() throws JsonProcessingException {
+
+        var refreshRoleRequest = new RefreshRoleRequest("",
+                Arrays.asList("test", "test"), Arrays.asList("test", "test"),null);
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+    }
+
+    @Test
+    void test_refreshUserProfile_Two_Input_04() throws JsonProcessingException {
+
+        var refreshRoleRequest = new RefreshRoleRequest("",
+                Arrays.asList("test", "test"), null,Arrays.asList("test", "test"));
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+    }
+
+    @Test
+    void test_refreshUserProfile_Multiple_Input() throws JsonProcessingException {
+
+        var refreshRoleRequest = new RefreshRoleRequest("cmc",
+                Arrays.asList("test", "test"), Arrays.asList("test", "test"),null);
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+    }
+
+    @Test
+    void test_refreshUserProfile_No_Input() throws JsonProcessingException {
+        checkAssertion("");
+    }
+
+    private void checkAssertion(String ccdServiceNames) {
+        var refreshRoleRequest = new RefreshRoleRequest(ccdServiceNames,
+                null, null,null);
+        Assertions.assertThrows(InvalidRequestException.class, () -> {
+            elinkUserService.refreshUserProfile(refreshRoleRequest, 1,
+                    0, "ASC", "objectId");
+        });
+    }
+
+
 
 }
