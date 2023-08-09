05bc9f64042afa4ca4705066cea5f6ce25c73198
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-08-09 14:36:48 +0100

    DTSRD-644: Update Judicial_ticket_code_mapping static table for Authorisations

--- src/integrationTest/resources/db/testmigration/V1_74__elinks_insert_ticket_code_mapping.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO dbjudicialdata.judicial_ticket_code_type (ticket_code,ticket_category_id,lower_level)
-VALUES ('379', 'Authorisation Tribunals', 'Others - Employment Tribunal England & Wales');
-INSERT INTO dbjudicialdata.judicial_ticket_code_type (ticket_code,ticket_category_id,lower_level)
-VALUES ('379', 'Authorisation Tribunals', 'Others - Employment Tribunal Scotland');
-
-COMMIT;
