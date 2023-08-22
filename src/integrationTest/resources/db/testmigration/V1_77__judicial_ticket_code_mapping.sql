04540ce588ea3cc23fd99c0fd7cb74e5d4227846
hmcts <prudhvi.maddineni@hmcts.net>
2023-08-22 12:17:04 +0100

    flyway changes

--- src/integrationTest/resources/db/testmigration/V1_77__judicial_ticket_code_mapping.sql
+++ src/integrationTest/resources/db/testmigration/V1_77__judicial_ticket_code_mapping.sql
@@ -3,3 +3,7 @@ INSERT INTO judicial_ticket_code_mapping (ticket_code,jurisdiction,lower_level)
 VALUES ('380', 'Authorisation Tribunals', 'Others - Employment Tribunal (Scotland)');
 INSERT INTO judicial_ticket_code_mapping (ticket_code,jurisdiction,lower_level)
 VALUES ('1413', 'Authorisation Tribunals','Others - Employment Tribunal England & Wales');
+
+
+INSERT INTO dbjudicialdata.jrd_lrd_region_mapping (jrd_region_id,jrd_region,region_id,region) VALUES
+    ('71',	'Cambridge',          '5',	'South East');
