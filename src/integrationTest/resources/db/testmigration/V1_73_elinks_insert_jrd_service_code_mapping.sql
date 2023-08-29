63ddd916a25c1ffffefe36ccbdd36e8feb2e6b1e
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-08-29 15:27:10 +0100

    DTSRD-1144.Updated Service mapping table for PRIVATE LAW

--- src/integrationTest/resources/db/testmigration/V1_73_elinks_insert_jrd_service_code_mapping.sql
+++ src/integrationTest/resources/db/testmigration/V1_73_elinks_insert_jrd_service_code_mapping.sql
@@ -4,6 +4,6 @@ INSERT INTO dbjudicialdata.judicial_service_code_mapping (service_id,ticket_code
 COMMIT;
 
 INSERT INTO dbjuddata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
-	 ((select service_id from dbjuddata.judicial_service_code_mapping order by service_id  desc limit 1)+1,'405','ABA5','Family Private Law');
+	 ('124','405','ABA5','Family Private Law');
 
 COMMIT;
