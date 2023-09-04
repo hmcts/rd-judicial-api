50a4b2673e3e8795011a71cf81f6dbb20c75de98
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-04 20:35:15 +0100

    DTSRD-1173- update audit.json

--- src/integrationTest/resources/db/testmigration/V1_79__judicial_service_code_mapping.sql
+++ src/integrationTest/resources/db/testmigration/V1_79__judicial_service_code_mapping.sql
@@ -1,8 +1,8 @@
 INSERT INTO dbjudicialdata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
-	 ((select service_id from dbjudicialdata.judicial_service_code_mapping order by service_id  desc limit 1)+1,'405','ABA5','Family Private Law');
+    ('123','405','ABA5','Family Private Law');
 
 
 INSERT INTO dbjuddata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
-	 ('124','405','ABA5','Family Private Law');
+    ('124','405','ABA5','Family Private Law');
 
 COMMIT;
