d51e89bf238b9630a272d398754772e56b648908
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-05 12:07:40 +0100

    DTSRD-1144.

--- src/integrationTest/resources/db/testmigration/V1_81__judicial_service_code_mapping.sql
+++ src/integrationTest/resources/db/testmigration/V1_81__judicial_service_code_mapping.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO dbjuddata.judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
+INSERT INTO judicial_service_code_mapping (service_id,ticket_code,service_code,service_description) VALUES
     ('124','405','ABA5','Family Private Law');
 
 COMMIT;
