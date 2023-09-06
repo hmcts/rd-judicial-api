8bb8bf4b082f8e48621aee2dd02ac6514ded0390
hmcts <prudhvi.maddineni@hmcts.net>
2023-09-06 09:57:12 +0100

    search change

--- src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/request/UserSearchRequest.java
+++ src/main/java/uk/gov/hmcts/reform/judicialapi/elinks/controller/request/UserSearchRequest.java
@@ -20,7 +20,7 @@ public class UserSearchRequest {
     @JsonProperty("searchString")
     @NotNull(message = "cannot be null")
     @NotEmpty(message = "cannot be empty")
-    @Pattern(regexp = "^[(a-zA-Z0-9 )\\p{L}\\p{N}'’-]{3,}$", message = "must be at least 3 characters including "
+    @Pattern(regexp = "^[(a-zA-Z0-9 )\\p{L}\\p{N}'’-]{3,}", message = "must be at least 3 characters including "
             + "letters, apostrophe, hyphen")
     @Schema(name = "searchString",  example = "string")
     private String searchString;
