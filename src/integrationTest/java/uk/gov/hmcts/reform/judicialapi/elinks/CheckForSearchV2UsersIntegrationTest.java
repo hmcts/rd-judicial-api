8db6bb96306e0438d8617be2bbddf435ca044da8
hmcts <prudhvi.maddineni@hmcts.net>
2023-09-06 10:45:52 +0100

    search change

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/CheckForSearchV2UsersIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/CheckForSearchV2UsersIntegrationTest.java
@@ -266,7 +266,7 @@ class CheckForSearchV2UsersIntegrationTest extends AuthorizationEnabledIntegrati
                 .build();
         var response = judicialReferenceDataClient.searchUsers(
                 userSearchRequest, role, false, MediaType.valueOf(V2.MediaType.SERVICE));
-        assertThat(response).containsEntry("http_status", "200");
+        assertThat(response).containsEntry("http_status", "200 OK");
         var responseBody = (String) response.get("response_body");
         assertTrue(responseBody.contains("searchString must be at least 3 characters including letters, "
                 + "apostrophe, hyphen"));
