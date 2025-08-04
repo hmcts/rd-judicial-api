690c2784f3941df62cf06e531e2ad5f83443df7c
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-08-04 14:34:47 +0100

    DTSRD-4072 committign functiuonal test

--- src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/config/TestConfigProperties.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/judicialapi/config/TestConfigProperties.java
@@ -23,8 +23,6 @@ public class TestConfigProperties implements TestConfig {
     @Value("${oauth2.client.secret}")
     public String clientSecret;
 
-    @Value("${test.user.password}")
-    public String testUserPassword;
 
     @Value("${idam.api.url}")
     public String idamApiUrl;
