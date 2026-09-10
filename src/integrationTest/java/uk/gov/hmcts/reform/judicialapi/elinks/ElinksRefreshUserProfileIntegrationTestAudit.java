0a3f19d92b8aa7a816daa42d33fd9e0d97d68cd5
Lukasz Krzysztof Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2026-09-10 17:56:42 +0100

    local factory method source reference

--- src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/ElinksRefreshUserProfileIntegrationTestAudit.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/judicialapi/elinks/ElinksRefreshUserProfileIntegrationTestAudit.java
@@ -48,8 +48,7 @@ public class ElinksRefreshUserProfileIntegrationTestAudit extends ElinksDataLoad
 
     @DisplayName("Non-Tribunal cft region and location")
     @ParameterizedTest
-    @MethodSource("uk.gov.hmcts.reform.judicialapi.elinks.util.ElinksDataLoadBaseTest"
-            + "#provideRefreshUserProfileTestData")
+    @MethodSource("provideRefreshUserProfileTestData")
     void shouldReturn200NonTribunalScenario(String serviceCode,
                                             String serviceName,
                                             int expectedUserProfilesCount) throws JsonProcessingException {
